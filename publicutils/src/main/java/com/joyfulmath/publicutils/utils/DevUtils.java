7f16cb355d66eed7ba152c22843a90139cdcb684
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-05-03 17:50:21 +0800

    update version

--- publicutils/src/main/java/com/joyfulmath/publicutils/utils/DevUtils.java
+++ publicutils/src/main/java/com/joyfulmath/publicutils/utils/DevUtils.java
@@ -1,6 +1,9 @@
 package com.joyfulmath.publicutils.utils;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.joyfulmath.publicutils.BuildConfig;
 
@@ -9,15 +12,19 @@ import com.joyfulmath.publicutils.BuildConfig;
  * version code,
  * debug flag to switch channel
  * blog flag to trace log when at release env.
+ *
  * @auther deman
  * Created on 2016/4/3.
  */
 public class DevUtils {
 
-    private static final String mVersionName = BuildConfig.VERSION_NAME;
-    private static final int mVersionCode = BuildConfig.VERSION_CODE;
+    private static String mVersionName;
+    private static int mVersionCode;
     private static final boolean bDebug = BuildConfig.DEBUG;
     private static boolean LOG_CONFIG = BuildConfig.LOG_CONFIG;
+    private static Context mContext = null;
+
+    public static String BuildTime = "";
 
     public static int getVersionCode() {
         return mVersionCode;
@@ -28,7 +35,6 @@ public class DevUtils {
     }
 
     /**
-     *
      * @return
      */
     public static boolean isDebug() {
@@ -40,9 +46,34 @@ public class DevUtils {
         TraceLog.setFlag(LOG_CONFIG);
     }
 
-    public static void init()
-    {
+    public static void init(Context context) {
+        mContext = context;
         TraceLog.setFlag(LOG_CONFIG);
+
+
+        try {
+            PackageManager manager = mContext.getPackageManager();
+            PackageInfo info = null;
+            info = manager.getPackageInfo(mContext.getPackageName(), 0);
+
+            // app版本
+            if (info != null) {
+                mVersionName = info.versionName;
+            }
+
+            // app内部版本
+            if (info != null) {
+                mVersionCode = info.versionCode;
+            }
+
+            PackageManager localPackageManager = mContext.getPackageManager();
+            ApplicationInfo localApplicationInfo = localPackageManager.getApplicationInfo(
+                    mContext.getPackageName(), 128);
+            BuildTime = String.valueOf(localApplicationInfo.metaData.get("BUILD_TIME"));
+
+        } catch (Exception e) {
+            // do nothing. not use the data
+        }
     }
 
 
